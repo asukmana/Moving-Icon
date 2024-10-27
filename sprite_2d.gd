539db183cb89328b0e68c7e3fc13d462eeaaeba7
asukmana <128885541+asukmana@users.noreply.github.com>
2024-10-27 07:56:14 +0700

    Move the icon based on user input

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -10,12 +10,32 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	#var velocity = Vector2.UP.rotated(rotation) * speed
 	
-	rotation += angular_speed * delta
+	#rotation += angular_speed * delta
+	
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+		
 	position += velocity * delta
 
 
 # Called when an instance of a class is created
 func _init():
 	print("hello from _init")
+
+# Called every time the player presses a key
+func _unhandled_input(event: InputEvent) -> void:
+	pass
+	
